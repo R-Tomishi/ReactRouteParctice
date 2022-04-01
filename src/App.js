55697db3f963627e4306907450bd15bc9cf32959
R-Tomishi <43924379+R-Tomishi@users.noreply.github.com>
2022-04-01 16:03:47 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page2 } from "./Page2";
-import { Page1DetailB } from "./Page2DetailB";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
@@ -16,31 +12,7 @@ export default function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              {console.log(url)}
-              <Route exact path="/page1">
-                <Page1 />
-              </Route>
-              <Route path="/page1/detailA">
-                <Page1DetailA />
-              </Route>
-              <Route path="/page1/detailB">
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
